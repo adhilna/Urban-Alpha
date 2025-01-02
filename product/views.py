f9aec67fe29ec58e89b68d6985cba6b3e0b7172f
adhilna <adhilchy11@gmail.com>
2025-01-02 20:52:10 +0530

    Update: Added homepage and product features

--- product/views.py
+++ product/views.py
@@ -4,11 +4,8 @@ from .models import *
 
 def all_products(request):
     products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'product/home.html', context)
-
-
-def categories(request):
     parent_categories = Category.objects.filter(parent__isnull=True)
-    context = {'parent_categories': parent_categories}
+    context = {'products': products, 'parent_categories': parent_categories}
     return render(request, 'product/home.html', context)
+
+
